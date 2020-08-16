1284fd2d37ad9ea5bc742db96684e920988df3ac
Glitch (fbsongsearch) <none>
2020-08-16 23:44:04 +0000

    💇🎆 Checkpoint ./app/fbme.py:21110574/525

---
+++ app/fbme.py
@@ -0,0 +1,13 @@
+import requests
+
+
+class fbme(object):
+    def is_downloadable(url):
+        h = requests.head(url, allow_redirects=True)
+        header = h.headers
+        content_type = header.get('content-type')
+        if 'text' in content_type.lower():
+            return False
+        if 'html' in content_type.lower():
+            return False
+        return True
