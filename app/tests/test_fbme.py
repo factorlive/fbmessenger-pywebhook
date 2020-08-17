84ed663b48f3e876fd0ad3932ee25e9a98dcea8e
Glitch (fbsongsearch) <none>
2020-08-17 23:02:30 +0000

    😹🔋 Checkpoint ./app/tests/test_fbme.py:21110574/200

--- app/tests/test_fbme.py
+++ app/tests/test_fbme.py
@@ -1,10 +1,14 @@
+import json
 from os import getenv
 from pathlib import Path
 from app.fbme import fb
 
 fixtures = Path(getenv('FIXTURES'))
 
+
 def test_audio_file_fbme():
-    h =
-    with 
+    h = fixtures / 'header_w_attach.json'
+    with open(h, 'r') as f:
+        header = json.load(f)
+    
     assert 1 == 1
