74fa2123b865e2cd166022fd85ef1f1831943a1e
Glitch (fbsongsearch) <none>
2020-08-18 14:30:22 +0000

    👘🐟 Checkpoint ./app/tests/test_fbme.py:21110574/308

--- app/tests/test_fbme.py
+++ app/tests/test_fbme.py
@@ -7,6 +7,14 @@ from app.fbme import fb
 
 fixtures = Path(getenv('FIXTURES'))
 
+@requests_mock.Mocker()
+def test_get_header_fbme():
+    h = fixtures / 'header_w_attach.json'
+    with open(h, 'r') as f:
+        header = json.load(f)
+    fb.get_header('http://test.com', text=header)
+    assert header 
+
 
 def test_audio_file_fbme():
     h = fixtures / 'header_w_attach.json'
