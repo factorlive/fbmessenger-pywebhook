73cfaf1b58b26eb35646b819d7889824200f7bec
Glitch (fbsongsearch) <none>
2020-08-18 17:01:59 +0000

    📅🏭 Checkpoint ./app/tests/test_fbme.py:21110574/280 ./app/fbme.py:21110574/236

--- app/fbme.py
+++ app/fbme.py
@@ -1,6 +1,6 @@
+from typing import Union
 from os import getenv
 from pathlib import Path
-import requests
 import logging
 
 # Logging toolbox 🔊
@@ -10,13 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 class Messenger(object):
-    def get_header(self, url):
-        return requests.head(url, allow_redirects=True)
-
-    def get_file(self, url):
-        return requests.get(url)
-        
-    def check_header(self, header):
+    def check_header(self, header) -> Union[str, None]:
         logger.info(header)
         audio_file = header.get('Content-Disposition')
         if 'mp4' in audio_file.split('.'):
--- app/tests/test_fbme.py
+++ app/tests/test_fbme.py
@@ -1,23 +1,20 @@
 import logging
 from pprint import pformat
-import json
-import requests
-from os import getenv
 from app.main import fb
 
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-    # requests_mock.get('http://fb.me', json=header)
-    # mocked_request = requests.get('http://fb.me')
+# requests_mock.get('http://fb.me', json=header)
+# mocked_request = requests.get('http://fb.me')
 
 
 def test_check_header_fbme(header_w_attach) -> None:
     test_filename = header_w_attach['Content-Disposition'].split('=')[1]
     assert test_filename == fb.check_header(header_w_attach)
 
-def test_check_header_fbme_wrong_filetype(header_w_attach):
+
+def test_check_header_fbme_wrong_filetype(header_w_attach) -> None:
     header_w_attach['Content-Disposition'] = 'other-file.wav'
     assert fb.check_header(header_w_attach) is None
-
