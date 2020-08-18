1e6cfe8c0444c9be2d553c834983ae09397d5660
Glitch (fbsongsearch) <none>
2020-08-18 15:28:32 +0000

    🐆👮 Checkpoint ./app/tests/test_fbme.py:21110574/336

--- app/tests/test_fbme.py
+++ app/tests/test_fbme.py
@@ -3,7 +3,7 @@ import json
 import requests
 from os import getenv
 from pathlib import Path
-from app.fbme import fb
+from app.main import fb
 
 fixtures = Path(getenv('FIXTURES'))
 
@@ -19,12 +19,11 @@ logger = logging.getLogger(__name__)
 #     assert header 
 
 
-def test_audio_file_fbme(requests_mock):
+def test_check_header_fbme(requests_mock):
     h = fixtures / 'header_w_attach.json'
     with open(h, 'r') as f:
         header = json.load(f)
-    logger.info(header)
     requests_mock.get('http://fb.me', json=header)
-    mocked_header = requests.get('http://fb.me')
-    logger.info(mocked_header.text)
-    assert 1 == 1
+    mocked_request = requests.get('http://fb.me')
+
+    assert header == 
